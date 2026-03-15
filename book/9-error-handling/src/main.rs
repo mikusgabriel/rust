49a4e29646489f137744d3f3daf6b9dfa232b044
Gabriel Mikus <gabriel.mikus11@gmail.com>
2026-03-15 16:56:45 -0400

    chapter 9 example, custom error handling

---
+++ book/9-error-handling/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("Hello, world!");
+
+    let guess = Guess::new(40);
+    let guess_failure = Guess::new(110);
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
